a0c9ec8eedaf2bbadbd784009997654f9013f2b5
obrens <starovico@gmail.com>
2018-08-13 21:01:58 +0200

    Ne poz, nego json

--- bekend/src/main/java/sesta/projekat/controller/TipKategorijeController.java
+++ bekend/src/main/java/sesta/projekat/controller/TipKategorijeController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import sesta.projekat.model.TipSmestaja;
 
 @RestController
 @RequestMapping("/tip")
@@ -11,6 +12,9 @@ public class TipKategorijeController {
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/poz")
 	public ResponseEntity hello(){
-		return ResponseEntity.ok("poz");
+		TipSmestaja t = new TipSmestaja();
+		t.setNaziv("okej");
+		t.setId(134l);
+		return ResponseEntity.ok(t);
 	}
 }
--- klijent/src/main/webapp/page/pageDelovi/pocetnaGostCtrl.js
+++ klijent/src/main/webapp/page/pageDelovi/pocetnaGostCtrl.js
@@ -1,7 +1,11 @@
 livadeApp.controller('pocetnaGostCtrl', ['$scope','$http', function ($scope,$http) {
     $scope.title="vuko";
 
-    $http.get("http://localhost:8080/abc/runlist")
+    /*$http.get("http://localhost:8080/tip/poz").success(function (data) {
+        alert("ajaj");
+        $scope.content = data;
+    });*/
+    $http.get("http://localhost:8080/tip/poz")
         .then(function(response) {
             $scope.content = response.data;
         }, function(response) {
