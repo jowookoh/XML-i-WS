9f98fa89e10f920fcc48480751558884fcca5bcd
vukojovicic <35064939+vukojovicic@users.noreply.github.com>
2018-08-13 19:11:07 +0200

    klija test

--- klijent/src/main/webapp/page/pageDelovi/pocetnaGostCtrl.js
+++ klijent/src/main/webapp/page/pageDelovi/pocetnaGostCtrl.js
@@ -1,3 +1,10 @@
-livadeApp.controller('pocetnaGostCtrl', ['$scope', function ($scope) {
+livadeApp.controller('pocetnaGostCtrl', ['$scope','$http', function ($scope,$http) {
     $scope.title="vuko";
+
+    $http.get("http://localhost:8080/abc/runlist")
+        .then(function(response) {
+            $scope.content = response.data;
+        }, function(response) {
+            $scope.content = "Something went wrong";
+        });
 }]);
