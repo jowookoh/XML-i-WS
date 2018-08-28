7102084fc3f188630c997df55a3a7f89d1a338fc
obrens <starovico@gmail.com>
2018-08-28 17:43:13 +0200

    Adminska inicijalizovana

---
+++ admin/src/main/webapp/page/pageDelovi/pocetnaGostCtrl.js
@@ -0,0 +1,24 @@
+livadeApp.controller('pocetnaGostCtrl', ['$scope','$http', function ($scope,$http) {
+
+    $scope.pretraga={}
+    $scope.napredna = false;
+    $scope.ngNapredna = function(flag) {
+        if (flag) {
+            $scope.napredna = false;
+        } else {
+            $scope.napredna = true;
+        }
+    };
+
+
+
+    $http.get("http://localhost:8080/tip/poz/")
+        .then(function(response) {
+            $scope.sara=response.data;
+            $scope.result = "Success";
+            $scope.content = response;
+        }, function(response) {
+            $scope.result = "Error";
+            $scope.content = response;
+        });
+}]);
