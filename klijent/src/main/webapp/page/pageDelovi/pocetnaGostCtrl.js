e9f913437b6272760eb47e20cfd63833d3755623
vukojovicic <35064939+vukojovicic@users.noreply.github.com>
2018-09-01 17:55:09 +0200

    pretraga sa filterima i svime

--- klijent/src/main/webapp/page/pageDelovi/pocetnaGostCtrl.js
+++ klijent/src/main/webapp/page/pageDelovi/pocetnaGostCtrl.js
@@ -20,9 +20,68 @@ livadeApp.controller('pocetnaGostCtrl', ['$scope','$state','$http', function ($s
         $state.go("mojeRezervacije");
     }
 
+    $scope.switchKrit = function(krit) {
+        $scope.kriter = krit;
+    }
+    $scope.switchPretip = function(pret) {
+        $scope.pretip = pret;
+    }
+    $scope.switchPrekat = function(prek) {
+        $scope.prekat = prek;
+    }
+    //#region checkboxovi
+
+    var parkto= true;
+    var vifito= true;
+    var dorto= true;
+    var poluto= true;
+    var panto= true;
+    var tvto= true;
+    var kuhto= true;
+    var kupto= true;
+
+
+    $scope.switchMod1 = function() {
+        if (parkto){$scope.mod1="parking"; parkto= !parkto; }
+        else{ $scope.mod1=""; parkto= !parkto;}
+    }
+    $scope.switchMod2 = function() {
+        if (vifito){$scope.mod2="wifi"; vifito= !vifito; }
+        else{ $scope.mod2="";vifito= !vifito; }
+    }
+    $scope.switchMod3 = function() {
+        if (dorto){$scope.mod3="dorucak"; dorto= !dorto; }
+        else{ $scope.mod3=""; dorto= !dorto; }
+    }
+    $scope.switchMod4 = function() {
+        if (poluto){$scope.mod4="polupanison"; poluto= !poluto; }
+        else{ $scope.mod4="";  poluto= !poluto;}
+    }
+    $scope.switchMod5 = function() {
+        if (panto){$scope.mod5="pansion"; panto= !panto; }
+        else{ $scope.mod5=""; panto= !panto;}
+    }
+    $scope.switchMod6 = function() {
+        if (tvto){$scope.mod6="tv"; tvto= !tvto; }
+        else{ $scope.mod6=""; tvto= !tvto;}
+    }
+    $scope.switchMod7 = function() {
+        if (kuhto){$scope.mod7="kuhinja"; kuhto= !kuhto; }
+        else{ $scope.mod7=""; kuhto= !kuhto;}
+    }
+    $scope.switchMod8 = function() {
+        if (kupto){$scope.mod8="privatno kupatilo"; kupto= !kupto; }
+        else{ $scope.mod8="";  kupto= !kupto;}
+    }
+    //#endregion checkboxovi
 
     $scope.pretrazi = function () {
-        alert($scope.pretraga);
+        $http.post("http://localhost:8080/smestaj/trazi/",$scope.pretraga)
+            .then(function(response) {
+                $scope.stigle=response.data;
+            }, function(response) {
+
+            });
     };
 
     /*$http.get("http://localhost:8080/tip/poz/")
@@ -33,7 +92,7 @@ livadeApp.controller('pocetnaGostCtrl', ['$scope','$state','$http', function ($s
         }, function(response) {
             $scope.result = "Error";
             $scope.content = response;
-        });*/
+        });
     $http({
         method: 'GET',
         url: "http://localhost:8080/tip/poz/"
@@ -44,5 +103,5 @@ livadeApp.controller('pocetnaGostCtrl', ['$scope','$state','$http', function ($s
     }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
-    });
+    });*/
 }]);
