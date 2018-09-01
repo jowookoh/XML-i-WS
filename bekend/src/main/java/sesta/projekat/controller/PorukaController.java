79a37ecc72062fc2b2e04d5461f069b52a927600
sijera94 <sijera94@gmail.com>
2018-09-01 21:39:12 +0200

    Uradjeno preuzimanje svih poruka koje su vezane za neku rezervaciju

--- bekend/src/main/java/sesta/projekat/controller/PorukaController.java
+++ bekend/src/main/java/sesta/projekat/controller/PorukaController.java
@@ -2,12 +2,14 @@ package sesta.projekat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sesta.projekat.model.Poruka;
+import sesta.projekat.model.Rezervacija;
 import sesta.projekat.service.PorukaService;
+import sesta.projekat.service.RezervacijaService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/poruka")
@@ -15,12 +17,27 @@ public class PorukaController {
 
     @Autowired
     private PorukaService porukaService;
+    @Autowired
+    private RezervacijaService rezervacijaService;
 
 
-    @RequestMapping(method = RequestMethod.POST, value = "/dodajPoruku")
+    @RequestMapping(method = RequestMethod.POST, value = "/nova")
     public ResponseEntity novaPoruka(@RequestBody Poruka poruka){
 
         Poruka porukaSacuvana = porukaService.save(poruka);
         return ResponseEntity.ok(porukaSacuvana);
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/porukeRezervacije/{id}")
+    public ResponseEntity getPorukeRezervacije(@PathVariable Long id) {
+        List<Poruka> tempPoruke = new ArrayList<>();
+        List<Poruka> svePoruke = porukaService.getAll();
+        for(Poruka poruka : svePoruke){
+            if(poruka.getRezervacija().getId() == id){
+                tempPoruke.add(poruka);
+            }
+        }
+        return ResponseEntity.ok(tempPoruke);
+    }
+
 }
